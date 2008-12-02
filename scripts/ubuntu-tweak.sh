991764de85dd9c0bb01d105d1ac9a9f9531fcc3c
billy3321 <billy3321>
2008-12-02 10:55:36 +0000

    將新增apt source之功能寫入add-apt-sources模組， 有需要之script可自行取用 並修改ubuntu-tweak.sh medibuntu為使用此模組
    修改replace-apt-sources使其自動偵測source.list中是否擁有medibuntu與ubuntu tweak安裝來源
    並在修改source.list後重新增加回去。

--- scripts/ubuntu-tweak.sh
+++ scripts/ubuntu-tweak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (bill3321,雨蒼) <bill3321 -AT- gmail.com>
-# Last Modified: 23 Oct 2008
+# Last Modified: 2 Dec 2008
 # Released under GNU General Public License
 # Add ubuntu tweak source into source.list and install ubuntu tweak from apt
 # Please run as root.
@@ -9,18 +9,8 @@
 
 
 #Check if source.list has source of ubuntu tweak
-HAS_UBUNTU_TWEAK=`grep 'deb http://ppa.launchpad.net/tualatrix/ubuntu' /etc/apt/sources.list`
-if [ -z "$HAS_UBUNTU_TWEAK" ]; then
-    if [ "$DISTRIB_CODENAME" = 'intrepid' ];then
-        echo -e "\n#the source of Ubuntu Tweak \ndeb http://ppa.launchpad.net/tualatrix/ubuntu intrepid main \ndeb-src http://ppa.launchpad.net/tualatrix/ubuntu intrepid main" >> /etc/apt/sources.list
-    else
-        echo -e "\n#the source of Ubuntu Tweak \ndeb http://ppa.launchpad.net/tualatrix/ubuntu hardy main \ndeb-src http://ppa.launchpad.net/tualatrix/ubuntu hardy main" >> /etc/apt/sources.list
-    fi
-#    wget -q http://ppa.launchpad.net/tualatrix/??-key.gpg -O- | sudo apt-key add - 
-     sudo apt-get update
-else
-    echo 'Ubuntu Tweak 套件庫已經存在'
-fi
+. scripts/add-apt-sources
 
+add_ubuntu_tweak
 
 apt-get -y --force-yes install ubuntu-tweak
