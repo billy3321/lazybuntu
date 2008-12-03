7c59de7e3fa84abbd647225fb91a97b7da2c71da
billy3321 <billy3321>
2008-12-03 12:27:38 +0000

    修改adobe安裝,使其支援中文字型

--- scripts/skype.sh
+++ scripts/skype.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (bill3321,雨蒼) <bill3321 -AT- gmail.com>
-# Last Modified: 25 Nov 2008
+# Last Modified: 3 Dec 2008
 # Released under GNU General Public License
 # Download and install skype for i686 and x86_64 ubuntu
 # Please run as root.
