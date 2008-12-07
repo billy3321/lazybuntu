184c3a0bacd9c9fc85f220a6010edf9fdb0427b9
billy3321 <billy3321>
2008-12-07 02:31:07 +0000

    盡量使用來自官方的套件 重新修改skype.sh使使用官方套件。

--- scripts/skype.sh
+++ scripts/skype.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (bill3321,雨蒼) <bill3321 -AT- gmail.com>
-# Last Modified: 3 Dec 2008
+# Last Modified: 7 Dec 2008
 # Released under GNU General Public License
 # Download and install skype for i686 and x86_64 ubuntu
 # Please run as root.
@@ -14,31 +14,31 @@ i686)
 scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu'
 ;;
 x86_64)
-# 將以下網址之內容執行於下
-# http://ubuntuforums.org/showthread.php?t=432295
-# case "$DISTRIB_CODENAME" in
-# intrepid)
-# apt-get -y --force-yes install ia32-libs lib32asound2 libasound2-plugins
-# scripts/download-install getlibs 'http://boundlesssupremacy.com/Cappy/getlibs/getlibs-all.deb'
-# scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
-# getlibs --yes -p libqtcore4 libqtgui4 bluez-alsa
-# ;;
-# hardy)
-# apt-get -y --force-yes install ia32-libs lib32asound2 libasound2-plugins
-# scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
-# ;;
-# gutsy|feisty|edgy)
-# apt-get -y --force-yes install ia32-libs lib32asound2
-# scripts/download-install getlibs 'http://boundlesssupremacy.com/Cappy/getlibs/getlibs-all.deb'
-# scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
-# getlibs --yes /usr/bin/skype
-#安裝來自medibuntu之skype
 
  . add-apt-sources
  
  add_medibuntu
+# 將以下網址之內容執行於下
+# http://ubuntuforums.org/showthread.php?t=432295
+ case "$DISTRIB_CODENAME" in
+ intrepid)
+ apt-get -y --force-yes install ia32-libs lib32asound2 libasound2-plugins
+ scripts/download-install getlibs 'http://boundlesssupremacy.com/Cappy/getlibs/getlibs-all.deb'
+ scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
+ getlibs --yes -p libqtcore4 libqtgui4 bluez-alsa
+ ;;
+ hardy)
+ apt-get -y --force-yes install ia32-libs lib32asound2 libasound2-plugins
+ scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
+ ;;
+ gutsy|feisty|edgy)
+ apt-get -y --force-yes install ia32-libs lib32asound2
+ scripts/download-install getlibs 'http://boundlesssupremacy.com/Cappy/getlibs/getlibs-all.deb'
+ scripts/download-install skype 'http://www.skype.com/go/getskype-linux-ubuntu-amd64'
+ getlibs --yes /usr/bin/skype
+#安裝來自medibuntu之skype
 
- apt-get install non-free-codecs skype
+# apt-get install non-free-codecs skype
 
  ;;
  *)
