8923541b799331fce6c79a9010465a3fc5f9df08
billy3321 <billy3321>
2008-12-27 19:10:56 +0000

    Make add_official_repos.py can add the apt source that added by lazybuntu

--- scripts/add_official_repos.py
+++ scripts/add_official_repos.py
@@ -30,6 +30,17 @@ def main ():
         print "your distribution is remix release!"
         return -1
 
+    for source in sourceslist:
+     if source.disabled == False:
+      if source.uri == "http://packages.medibuntu.org/":
+       has_medibuntu_source = 1
+      if source.uri == "http://ppa.launchpad.net/tualatrix/ubuntu":
+       has_ubuntu_tweak_source = 1
+      if source.uri == "http://getswiftfox.com/builds/debian":
+       has_swiftfox_source = 1
+      if source.uri == "http://wine.budgetdedicated.com/apt":
+       has_winehq_source = 1
+
     [entry.set_enabled (False) for entry in sourceslist if entry.invalid == False]
 
     if distro.country_code == "tw" and distro.id == "Ubuntu":
@@ -43,6 +54,30 @@ def main ():
 
     distro.add_source (uri=security_uri, dist=security_dist, comps=comps, comment="安全性更新伺服器 (Lazybuntu 新增)")
     distro.add_source (type="deb-src", uri=security_uri, dist=security_dist, comps=comps, comment="安全性更新伺服器 (Lazybuntu 新增)")
+
+    if has_medibuntu_source:
+       medibuntu_uri = "http://packages.medibuntu.org/"
+       medibuntu_comps = ["free non-free"]
+       distro.add_source (uri=medibuntu_uri, comps=medibuntu_comps, comment="Medibuntu Install Source")
+    if has_ubuntu_tweak_source:
+       ubuntu_tweak_uri = "http://ppa.launchpad.net/tualatrix/ubuntu"
+       ubuntu_tweak_comps = ["main"]
+       if distro.codename == 'intrepid':
+          ubuntu_tweak_dist = distro.codename
+       else:
+          ubuntu_tweak_dist = 'hardy'
+       distro.add_source (uri=ubuntu_tweak_uri, dist=ubuntu_tweak_dist, comps=ubuntu_tweak_comps, comment="Ubuntu Tweak Install Source")
+       distro.add_source (type="deb-src", uri=ubuntu_tweak_uri, dist=ubuntu_tweak_dist, comps=ubuntu_tweak_comps, comment="Ubuntu Tweak Install Source")
+    if has_swiftfox_source:
+       swiftfox_uri = "http://getswiftfox.com/builds/debian"
+       swiftfox_comps = ["non-free"]
+       swiftfox_dist = "unstable"
+       distro.add_source (uri=swiftfox_uri, dist=swiftfox_dist, comps=swiftfox_comps, comment="Swiftfox Install Source")
+    if has_winehq_source:
+       winehq_uri = "http://wine.budgetdedicated.com/apt"
+       winehq_comps = ["main"]
+       distro.add_source (uri=winehq_uri, comps=winehq_comps, comment="WineHQ Install Source")
+
 
     sourceslist.backup ()
     sourceslist.save ()
